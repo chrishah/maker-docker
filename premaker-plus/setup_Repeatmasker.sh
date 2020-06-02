939c6d7563d9f7b4121c5233001819ea40e5ef3f
chrishah <chrisi.hahni@gmail.com>
2020-06-02 12:02:07 +0200

    add metadata check for Repeat libraries

--- premaker-plus/setup_Repeatmasker.sh
+++ premaker-plus/setup_Repeatmasker.sh
@@ -8,5 +8,20 @@ repeattarball=$2
 #take care of RepeatMasker
 cp -pfr /usr/local/RepeatMasker $destination
 tar xvfz $repeattarball -C $destination/RepeatMasker/
-$destination/RepeatMasker/rebuild
+#cd $destination/RepeatMasker
+#perl ./rebuild
+#cd -
+
+cd $destination/RepeatMasker
+
+#
+echo "Checking Repbase metadata against sequence data"
+cat Libraries/RMRBSeqs.embl | grep "^ID " | sed 's/^ID   //' | cut -d " " -f 1 | sort -n | uniq | perl -ne 'chomp; print "$_\n$_\n"' > comp1
+cat Libraries/RMRBMeta.embl | grep "^ID " | sed 's/^ID   //' | cut -d " " -f 1 | sort -n | uniq > comp2
+cat comp1 comp2 | sort -n | uniq -c | grep "   1 " | sed 's/^ .*1 //' > missing
+for m in $(cat missing); do sed -i "/$m/,/\/\//d" Libraries/RMRBMeta.embl; done
+rm comp1 comp2 missing
+
+#rebuild the repeat database
+perl ./rebuild
 
